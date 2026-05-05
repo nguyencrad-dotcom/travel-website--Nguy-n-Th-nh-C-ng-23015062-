8725f1496b0c620a67b38929e2b040230e83b850
Nguyễn Thành Công <nguyencrad@gmail.com>
2026-05-05 23:36:18 +0700

    level 1 travel website final

--- js/script.js
+++ js/script.js
@@ -1,29 +1,10 @@
-// SLIDER
-let images = [
-"https://images.unsplash.com/photo-1583417319070-4a69db38a482",
-"https://images.unsplash.com/photo-1597047084897-51e81819a499",
-"https://images.unsplash.com/photo-1573843981267-be1999ff37cd"
-];
-
-let i=0;
-setInterval(()=>{
-i++;
-if(i>=images.length)i=0;
-document.getElementById("slide").src=images[i];
-},3000);
-
-// POPUP
 function chonTour(ten){
-document.getElementById("popup").style.display="block";
-document.getElementById("popup-text").innerText="Bạn đã chọn "+ten;
+document.getElementById("thongbao").innerText=
+"Bạn đã chọn tour "+ten;
 }
 
-function dongPopup(){
-document.getElementById("popup").style.display="none";
-}
-
-// FORM
 function datTour(){
+
 let name=document.getElementById("name").value;
 let phone=document.getElementById("phone").value;
 let email=document.getElementById("email").value;
@@ -38,9 +19,4 @@ return false;
 
 document.getElementById("message").innerText="Đặt tour thành công";
 return false;
-}
-
-// DARK MODE
-function toggleDark(){
-document.body.classList.toggle("dark");
 }
