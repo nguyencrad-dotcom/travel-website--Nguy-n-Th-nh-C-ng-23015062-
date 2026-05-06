471d7dcf424da6f39816d6fe49d8d4221ffc6397
Nguyễn Thành Công <nguyencrad@gmail.com>
2026-05-06 07:59:38 +0700

    add php project

---
+++ php/baif1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Tour - Giới thiệu</title>
+</head>
+<body>
+    <h1>website du lịch</h1>
+    <?php
+    echo "<p>chào mừng bạn đến với website du lịch</p>" ;
+    echo "<p> Hotline: 0123456789</p>";
+    echo "<p> .......@gmail.com</p>";
+    ?>
+</body>
+</html>
